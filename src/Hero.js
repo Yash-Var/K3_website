b0e66bd913f9b62f626483e616fa32ac0d841c3a
KushagraVarshney05 <varkushqwerty@gmail.com>
2022-12-11 22:12:56 +0530

    updated home page

--- src/Hero.js
+++ src/Hero.js
@@ -10,13 +10,20 @@ const Hero = () => {
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-50">
-              Kiet Koder Korner
+            KIET Koders Korner &lt; &gt; Creating Competitive Coding Culture at Campus" [K3=C5]
             </h1>
             <p class="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+              Vision-
+
+              To make KIET Group of Institutions well known globally by achieving great heights among <br />various technical competitive platforms and outperforming in various<br/> international and national events."<br/>
+
+              Mission-<br/>
+
+              1. Make it to the ACM ICPC Finale "Olympics of Programming Competitions"<br/>
+
+              2. To qualify and acquire various top-notch packages through hiring hackathons like Code <br/>Agon, Google CodeJam, etc.<br/>
+
+              3. To upscale the ranking of KIET nationally and internationally on various coding <br/>platforms.<br/>
             </p>
             <div class="flex justify-center">
               <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
@@ -29,7 +36,7 @@ const Hero = () => {
           </div>
           <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <picture>
-            <source class="object-cover object-center rounded" srcSet={logo_oo} media="(d)"/>
+              <source class="object-cover object-center rounded" srcSet={logo_oo} media="(d)" />
               <img
                 class="object-cover object-center rounded"
                 alt="hero"
