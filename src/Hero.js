dede687c33ec3f4b0a647c1f5452d392db2091fa
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-12-17 17:08:58 +0530

    some changed in the team page �

--- src/Hero.js
+++ src/Hero.js
@@ -21,7 +21,7 @@ const Hero = () => {
   return (
     <>
       <section class="text-gray-600 body-font dark:text-gray-400 dark:bg-gray-900 " id="home">
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div class="container mx-auto flex md:flex-row flex-col items-center">
           <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-50 slide-top">
             KIET Koders Korner &lt; &gt; Creating Competitive Coding Culture at Campus" [K3=C5]
